f30fc2a96fbfbd53d879a3f8bc321f8c215aa40b
tkwon <taejoongkwon@gmail.com>
2015-09-10 18:12:59 -0600

    adding initial commit

---
+++ Assets/scripts/GameControll.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameControll : MonoBehaviour {
+	public Camera cam;
+	public GameObject fall;
+	private float maxWidth;
+	public Text scoreText;
+	public float score;
+	// Use this for initialization
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+		Vector3 upperCorner = new Vector2 (Screen.width, Screen.height);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		maxWidth = targetWidth.x;
+		//fall = GameObject.Find ("fall");
+		StartCoroutine (Spawn ());
+		score = 0;
+
+
+	}
+
+	void FixedUpdate() {
+
+		//score += Time.deltaTime;
+		scoreText.text = "Score\n" + Mathf.RoundToInt (score);
+		//print ("score = " + score);
+	}
+
+	IEnumerator Spawn() {
+		yield return new WaitForSeconds (2.0f);
+		while (true) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-maxWidth, maxWidth), transform.localPosition.y, 0);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (fall, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds(Random.Range(0.0f, 1.0f - score/100));
+		}
+	}
+}
---
+++ Assets/scripts/destroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class destroy : MonoBehaviour {
+	public Text scoreText;
+	public int score;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		GameControll gameScript = GameObject.Find ("GameController").GetComponent<GameControll>();
+		gameScript.score += 1;
+		Destroy (other.gameObject);
+		
+
+	}
+}
