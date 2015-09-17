5a013d6578ccec494667f6360693e71b51cd1ca7
ByungJunKim <aska0711@gmail.com>
2015-09-17 01:01:31 -0600

    Set background scroll, object positions, camera size Need suhun to create transparent texture for wave

---
+++ Assets/scripts/MainCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(1080, 1920, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
