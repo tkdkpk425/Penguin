fd1e7035f9cca661e8ef507014edf51c28699606
ByungJunKim <aska0711@gmail.com>
2015-09-18 02:44:04 -0600

    Fixed depth of background, disabled screen rotation apk will not allow phones to rotate and will aloways keep vertial orientation during the game. also moved files to approperiate folders

--- Assets/scripts/MainCamera.cs
+++ Assets/scripts/MainCamera.cs
@@ -7,6 +7,7 @@ public class MainCamera : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Screen.SetResolution(1080, 1920, true);
+        Screen.orientation = ScreenOrientation.Portrait;
     }
 	
 	// Update is called once per frame
