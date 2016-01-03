e3f7759afc9cfc9462f1aab41afff8a09276d7de
myung kim <myungho.mortimen@gmail.com>
2016-01-03 16:57:46 -0700

    swipe in process

---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+	private int charcterNum;
+	private float speed;
+	private int size;
+	private string description;
+	public GameObject obj;
+	public readonly int Penguin = 0;
+	public readonly int Fox = 1;
+	public readonly int Walarus = 2;
+	public readonly int CharHolder = 3;
+	public readonly int CharHolder2 = 4;
+
+	// Use this for initialization
+	void Start () {
+//		obj = new GameObject ();
+//		obj.AddComponent<Canvas> ();		
+//		Canvas canvas = obj.GetComponent<Canvas> ();
+//		canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+//		canvas.AddComponenet<Text> ();
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public string getDescription() {
+		return description;
+	}
+
+	public void setCharacterNum(int num) {
+		this.charcterNum = num;
+	}
+
+	public int getCharacterNum() {
+		return charcterNum;
+	}
+}
---
+++ Assets/scripts/SwipeDetector.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SwipeDetector : MonoBehaviour 
+{
+	
+	public float minSwipeDistY;
+	
+	public float minSwipeDistX;
+
+	public Text Swipe;
+	public Image leftImage;
+	public Image centerImage;
+	public Image rightImage;
+	public Text charDescription;
+	private Vector2 startPos;
+	private int[] charNums;
+	void Start() 
+	{
+		Swipe.text = "default";
+		charNums = new int[]{0,1,2,3,4};
+	}
+	void Update()
+	{
+		//#if UNITY_ANDROID
+		if (Input.touchCount > 0) 
+			
+		{
+			
+			Touch touch = Input.touches[0];
+			
+			
+			
+			switch (touch.phase) 
+				
+			{
+				
+			case TouchPhase.Began:
+				
+				startPos = touch.position;
+				
+				break;
+				
+				
+				
+			case TouchPhase.Ended:
+				
+				float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
+				
+				if (swipeDistVertical > minSwipeDistY) 
+					
+				{
+					
+					float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
+					
+					if (swipeValue > 0)//up swipe
+					{
+
+					} else if (swipeValue < 0)//down swipe
+					{}		
+							//Shrink ();
+							
+				}
+				
+				float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+				
+				if (swipeDistHorizontal > minSwipeDistX) 
+				{
+					
+					float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+					
+					if (swipeValue > 0) {//right swipe
+						Swipe.text = "moving right";
+					} else if (swipeValue < 0)//left swipe
+					{		
+							//MoveLeft ();
+						Swipe.text = "moving left";
+					}
+					
+				}
+				break;
+			}
+		}
+	}
+}
